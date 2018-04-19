d163238a8c06e7ec2eea83b2f10af46dafdf937c
Jose <jmlb0003@gmail.com>
2018-04-19 23:08:28 +0200

    Updated model classes to fetch all the data from the json Added internet permissions

--- app/src/main/java/com/jamargle/bakineando/domain/model/Ingredient.java
+++ app/src/main/java/com/jamargle/bakineando/domain/model/Ingredient.java
@@ -4,21 +4,17 @@ import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.Ignore;
 import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
 
-import static com.jamargle.bakineando.domain.model.Step.TABLE_NAME;
+import static com.jamargle.bakineando.domain.model.Ingredient.TABLE_NAME;
 
 @Entity(tableName = TABLE_NAME)
 public final class Ingredient {
 
-    public static final String TABLE_NAME = "ingredients";
-    public static final String COLUMN_ID = "id";
+    public static final String TABLE_NAME = "Ingredients";
     public static final String COLUMN_QUANTITY = "quantity";
     public static final String COLUMN_MEASURE = "measure";
     public static final String COLUMN_INGREDIENT = "ingredient";
-
-    @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(index = true, name = COLUMN_ID)
-    private int id;
 
     @ColumnInfo(name = COLUMN_QUANTITY)
     private float quantity;
@@ -26,44 +22,52 @@ public final class Ingredient {
     @ColumnInfo(name = COLUMN_MEASURE)
     private String measure;
 
+    @PrimaryKey
+    @NonNull
     @ColumnInfo(name = COLUMN_INGREDIENT)
     private String ingredient;
 
+    public Ingredient() {
+        this.ingredient = "";
+        // Needed by Room setup
+    }
+
     @Ignore
     public Ingredient(final Builder builder) {
-        this.id = builder.id;
         this.quantity = builder.quantity;
         this.measure = builder.measure;
         this.ingredient = builder.ingredient;
-    }
-
-    public int getId() {
-        return id;
     }
 
     public float getQuantity() {
         return quantity;
     }
 
+    public void setQuantity(final float quantity) {
+        this.quantity = quantity;
+    }
+
     public String getMeasure() {
         return measure;
+    }
+
+    public void setMeasure(final String measure) {
+        this.measure = measure;
     }
 
     public String getIngredient() {
         return ingredient;
     }
 
+    public void setIngredient(final String ingredient) {
+        this.ingredient = ingredient;
+    }
+
     public static class Builder {
 
-        private int id;
         private float quantity;
         private String measure;
         private String ingredient;
-
-        public Builder id(final int id) {
-            this.id = id;
-            return this;
-        }
 
         public Builder quantity(final float quantity) {
             this.quantity = quantity;
--- app/src/main/java/com/jamargle/bakineando/domain/model/Step.java
+++ app/src/main/java/com/jamargle/bakineando/domain/model/Step.java
@@ -4,6 +4,7 @@ import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.Ignore;
 import android.arch.persistence.room.PrimaryKey;
+import com.google.gson.annotations.SerializedName;
 
 import static com.jamargle.bakineando.domain.model.Step.TABLE_NAME;
 
@@ -12,6 +13,7 @@ public final class Step {
 
     public static final String TABLE_NAME = "Steps";
     public static final String COLUMN_ID = "id";
+    public static final String COLUMN_STEP_NUMBER = "number";
     public static final String COLUMN_SHORT_DESCRIPTION = "short_description";
     public static final String COLUMN_DESCRIPTION = "description";
     public static final String COLUMN_VIDEO_URL = "video_url";
@@ -19,7 +21,12 @@ public final class Step {
 
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(index = true, name = COLUMN_ID)
-    private int id;
+    private int tableId;
+
+    @ColumnInfo(name = COLUMN_STEP_NUMBER)
+
+    @SerializedName("id")
+    private int stepNumber;
 
     @ColumnInfo(name = COLUMN_SHORT_DESCRIPTION)
     private String shortDescription;
@@ -33,25 +40,84 @@ public final class Step {
     @ColumnInfo(name = COLUMN_THUMBNAIL_URL)
     private String thumbnailURL;
 
+    public Step() {
+        // Needed by Room setup
+    }
+
     @Ignore
     public Step(final Builder builder) {
-        this.id = builder.id;
+        this.tableId = builder.tableId;
+        this.stepNumber = builder.stepNumber;
         this.shortDescription = builder.shortDescription;
         this.description = builder.description;
         this.videoURL = builder.videoURL;
         this.thumbnailURL = builder.thumbnailURL;
     }
 
+    public int getTableId() {
+        return tableId;
+    }
+
+    public void setTableId(final int tableId) {
+        this.tableId = tableId;
+    }
+
+    public int getStepNumber() {
+        return stepNumber;
+    }
+
+    public void setStepNumber(final int stepNumber) {
+        this.stepNumber = stepNumber;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(final String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public String getVideoURL() {
+        return videoURL;
+    }
+
+    public void setVideoURL(final String videoURL) {
+        this.videoURL = videoURL;
+    }
+
+    public String getThumbnailURL() {
+        return thumbnailURL;
+    }
+
+    public void setThumbnailURL(final String thumbnailURL) {
+        this.thumbnailURL = thumbnailURL;
+    }
+
     public static class Builder {
 
-        private int id;
+        private int tableId;
+        private int stepNumber;
         private String shortDescription;
         private String description;
         private String videoURL;
         private String thumbnailURL;
 
-        public Builder id(final int id) {
-            this.id = id;
+        public Builder tableId(final int tableId) {
+            this.tableId = tableId;
+            return this;
+        }
+
+        public Builder stepNumber(final int stepNumber) {
+            this.stepNumber = stepNumber;
             return this;
         }
 
