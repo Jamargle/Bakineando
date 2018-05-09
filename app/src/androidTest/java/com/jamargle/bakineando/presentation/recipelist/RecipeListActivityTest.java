0bb52b850f11a43e3f0be6e7839ca9286714a7d8
Jose <jmlb0003@gmail.com>
2018-05-10 00:35:09 +0200

    Removed unneded stuff in tests

--- app/src/androidTest/java/com/jamargle/bakineando/presentation/recipelist/RecipeListActivityTest.java
+++ app/src/androidTest/java/com/jamargle/bakineando/presentation/recipelist/RecipeListActivityTest.java
@@ -47,32 +47,10 @@ import static org.hamcrest.Matchers.any;
 @RunWith(AndroidJUnit4.class)
 public class RecipeListActivityTest {
 
-    private static final String EXTRA_RECIPE_ID_KEY = "RECIPE_ID";
-    private static final int EXTRA_RECIPE_ID_VALUE = 1;
-
     @Rule
     public ActivityTestRule<RecipeListActivity> testRule = new ActivityTestRule<>(RecipeListActivity.class);
 
     private IdlingResource idlingResource;
-
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
-    }
 
     @Before
     public void registerIdlingResource() {
@@ -148,6 +126,25 @@ public class RecipeListActivityTest {
                                 0)),
                         2)));
         servingsView.check(matches(isDisplayed()));
+    }
+
+    private Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 
 }
